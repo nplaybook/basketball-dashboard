453795bd4d86de98dbab2249f0101fb70528a00d
nplaybook <naufal.abdila@outlook.com>
2021-04-16 06:22:59 +0700

    move db initialization to init

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.team.team import team_bp
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://<user>:<pwd>@localhost:<port>/<database_name>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:110696@localhost:5432/basketball-dashboard"
 app.register_blueprint(team_bp, url_prefix="/team")
+
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run()
