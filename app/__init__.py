6f54ff96436b6943462515bdf0b90ae48cdf2ed6
nplaybook <naufal.abdila@outlook.com>
2021-04-18 05:53:57 +0700

    register player blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ sys.path.insert(0, parentdir)
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.player.player import player_bp
 from app.team.team import team_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:110696@localhost:5432/basketball-dashboard"
 app.register_blueprint(team_bp, url_prefix="/team")
+app.register_blueprint(player_bp, url_prefix="/player")
 
 db = SQLAlchemy(app)
 
