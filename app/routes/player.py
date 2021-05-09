c72171b698e72de8040dbe4cbea671e2a2e2837e
nplaybook <naufal.abdila@outlook.com>
2021-05-09 23:44:44 +0700

    restructure repo

---
+++ app/routes/player.py
@@ -0,0 +1,79 @@
+import time
+from flask import Blueprint, jsonify, request, redirect, url_for
+from ..orm.models import Player
+# from basketball_reference_scraper.players import get_stats
+# from basketball_reference_scraper.teams import get_roster
+
+player_bp = Blueprint(name="player_bp", import_name=__name__)
+
+@player_bp.route("/", methods=["GET"])
+def search():
+    if request.method == "GET":
+        try:
+            players = Player.query.all()
+            return jsonify({
+                "status": 200,
+                "message": "OK",
+                "query": players
+            })
+        except:
+            return jsonify({
+                "status": 404,
+                "message": "Data not found"
+            })
+        
+@player_bp.route("/<int:id>", methods=["GET"])
+def get_player_stats(id):
+    if request.method == "GET":
+        start = time.time()
+        try:
+            player = get_stats(name=name, stat_type="PER_GAME", playoffs=False, career=True)
+        except:
+            return jsonify({
+                "status": 505,
+                "message": "Fail fetch player information",
+                "execution_time": f"{round(time.time() - start, 2)} s"
+            })
+        else:
+            player_stats = {
+                "ppg": round(player["PTS"].mean(),1),
+                "apg": round(player["AST"].mean(),1),
+                "rpg": round(player["TRB"].mean(),1),
+                "spg": round(player["STL"].mean(),1),
+                "bpg": round(player["BLK"].mean(),1),
+                "tov": round(player["TOV"].mean(),1),
+                "pf": round(player["PF"].mean(),1)
+            }
+            return jsonify({
+                "status": 200,
+                "message": "OK",
+                "execution_time": f"{round(time.time() - start, 2)} s",
+                "query": player_stats
+                })
+
+@player_bp.route("/bio", methods=["GET"])
+def get_player_bio():
+    try:
+        team_roster = get_roster(team="CHI", season=2021)
+    except:
+        return jsonify({
+            "status": 505,
+            "message": "Fail fetch player information",
+            "execution_time": f"{round(time.time() - start, 2)} s"
+        })
+    else:
+        player_info = team_roster.query("PLAYER == 'Zach LaVine'")
+        player_info = {
+            "name": player_info["PLAYER"].values[0],
+            "jersey": int(player_info["NUMBER"].values[0]),
+            "pos": player_info["POS"].values[0],
+            "height": player_info["HEIGHT"].values[0],
+            "weight": int(player_info["WEIGHT"].values[0]),
+            "experience": int(player_info["EXPERIENCE"].values[0])
+        }
+        return jsonify({
+            "status": 200,
+            "message": "OK",
+            "execution_time": f"{round(time.time() - start, 2)} s",
+            "query": player_info
+        })
